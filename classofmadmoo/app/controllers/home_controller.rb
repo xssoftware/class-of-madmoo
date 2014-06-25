83cb4591cee82926971f5903f2e369d4779e574b
nCuXaP <nupoha@gmail.com>
2014-06-25 02:27:24 +0000

    user dashboard/root stuff

--- classofmadmoo/app/controllers/home_controller.rb
+++ classofmadmoo/app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
     @teams = Team.all
+    if current_user.team_id != nil
+    	team = Team.find(current_user.team_id)
+    	@projects = Project.where(team_id: current_user.team_id)
+    	@work_assignments = {}
+    	@projects.all.each do |project|
+	    	@work_assignments = WorkAssignment.where(project_id: project.id)
+    	end
+    end
   end
 end
